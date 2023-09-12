e05bb053053e34fd5855e9ed6d5f96fd9535f928
rodrigodeleon <20547168+rodrigodeleon@users.noreply.github.com>
2023-09-12 15:40:02 +0200

    project setup

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/app.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Image Slider",
+    }),
+  ],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
